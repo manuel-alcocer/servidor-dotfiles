1f61b9807bcbe1158d67968e0473bd2ac5a2f19f
Manuel Alcocer Jiménez <manuel@alcocer.net>
2018-06-25 11:23:11 +0200

    latest

--- services/pdns_admin/utils/pdns_admin.sh
+++ services/pdns_admin/utils/pdns_admin.sh
@@ -2,6 +2,9 @@
 
 PDNSADMIN_CONF_LOCK='/root/pdns_admin_config.lock'
 PDNSADMIN_PATH='/opt/web/powerdns-admin'
+VENV_ACTIVATE="${PDNSADMIN_PATH}/flask/bin/activate"
+
+MYSQL_CMD="mysql -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_ROOT_PASSWORD} -P${MYSQL_PORT}"
 
 function check_config(){
     # si existe este fichero, indica que ya se ha configurado
@@ -13,15 +16,67 @@ function check_config(){
     fi
 }
 
+function flask_db_init(){
+    flask db init
+    [[ $? ]] && exit 1
+}
+
+function flask_db_migrate(){
+    flask db migrate -m "Init DB"
+    [[ $? ]] && exit 1
+}
+
+function flask_db_upgrade(){
+    flask db upgrade
+    [[ $? ]] && exit 1
+}
+
+function init_data(){
+    ./init_data.py
+    [[ $? ]] && exit 1
+}
+
+function yarn_install(){
+    yarn install --pure-lockfile
+    [[ $? ]] && exit 1
+}
+
+function build_assets(){
+    flask assets build
+    [[ $? ]] && exit 1
+}
+
 function init_flask(){
-    [[ -d  ]]
+    cd ${PDNSADMIN_PATH}
+    [[ -d 'migrations' ]] && rm -rf migrations
+    export FLASK_APP=app/__init__.py
+    flask_db_init
+    flask_db_migrate
+    init_data
+    yarn_install
+    build_assets
+}
+
+function database_init(){
+    $MYSQL_CMD -e "create database $SQLA_DB_NAME"
+    [[ $? ]] && printf 'No se pudo crear la base de datos.\nSaliendo...\n' && exit 1
+    $MYSQL_CMD -e "create user '${SQLA_DB_USER}'@'%' identified by '${SQLA_DB_PASSWORD}'"
+    [[ $? ]] && printf 'No se pudo crear el usuario.\nSaliendo...\n' && exit 1
+    $MYSQL_CMD -e "grant all on ${SQLA_DB_NAME}.* to '${SQLA_DB_USER}'@'%'"
+    [[ $? ]] && printf 'No se pudieron asignar privilegios.\nSaliendo...\n' && exit 1
 }
 
 function main(){
     if [[ $(check_config) ]]; then
+        source ${VENV_ACTIVATE}
+        database_init
         init_flask
+        touch ${PDNSADMIN_CONF_LOCK}
     fi
+    while true; do
+        sleep 10
+        :
+    done
 }
-while true; do
-    sleep 10
-done
+
+main
