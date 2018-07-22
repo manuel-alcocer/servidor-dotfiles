782c1e759abba241edd1db45c3d4fc047cf0f85c
Manuel Alcocer Jiménez <manuel@alcocer.net>
2018-07-22 16:25:07 +0200

    ldap terminado

--- services/builders/openldap/entrypoint.sh
+++ services/builders/openldap/entrypoint.sh
@@ -16,14 +16,30 @@
 # VARIABLES DEL DEMONIO:
 #   OPTS: Parámetros adicionales a pasar al comando 'slapd'
 #   DEBUG_MODE: entero definiendo el modo de depuración (-d? muestra una lista)
+#
+# RESTAURAR BASE DE DATOS Y CONFIGURACIÓN:
+# - Si existe el fichero "/restore-config.ldif" restaura la configuración
+# - Si existe el fichero "/restore-database.ldif" restaura la base de datos
+# - VARIABLES PARA RESTAURAR:
+#       - RESTORE_CONF_ALWAYS: Si 1, siempre inicializa la configuración
+#       - RESTORE_DB_ALWAYS: Si 1, siempre inicializa la BBDD.
+#
+#       (Por defecto si existe la BBDD no se hace nada con los ficheros)
+#
+#   NOTAS: Si se pasa alguno de los 2 ficheros nunca se crea la BBDD desde 0
 
 SLAPD_DB_DIR='/etc/openldap/slapd.d'
-PID_DIR='/run/openldap/'
+PID_DIR='/run/openldap'
 SLAPD_CONF='/etc/openldap/slapd.conf'
-DB_CONFIG='/var/lib/openldap/openldap-data/DB_CONFIG'
+SLAPD_CONF_DIR="$SLAPD_DB_DIR"
+DB_DIR='/var/lib/openldap/openldap-data'
+DB_CONFIG="${DB_DIR}"'/DB_CONFIG'
 
 # Opciones adicionales
 OPTS=""
+
+RESTORE_CONF_FILE='/restore-config.ldif'
+RESTORE_DB_FILE='/restore-database.ldif'
 
 function check_dirs(){
     [[ ! -d $PID_DIR ]] && mkdir $PID_DIR
@@ -74,6 +90,49 @@ function kill_slapd(){
     (( slapd_pid )) && kill -9 $slapd_pid
 }
 
+function remove_conf(){
+    printf 'Borrando configuración anterior...\n'
+    rm -rf $SLAPD_CONF_DIR
+    mkdir $SLAPD_CONF_DIR && chown ldap: -R $SLAPD_CONF_DIR
+}
+
+function restore_config(){
+    conf_lock="$SLAPD_CONF_DIR"'/.slapd_conf.lock'
+    [[ $RESTORE_CONF_ALWAYS -eq 1 || ! -f $conf_lock ]] && remove_conf
+    if [[ ! -f $conf_lock ]]; then
+        printf 'Restaurando directorio de configuración...\n'
+        su - -s /bin/bash ldap \
+            -c '/usr/sbin/slapadd -n 0 -F '"$SLAPD_CONF_DIR"' -l '"$RESTORE_CONF_FILE"
+        (( $? )) || touch $conf_lock
+    fi
+}
+
+function remove_db(){
+    printf 'Borrando base de datos anterior...\n'
+    rm -rf $DB_DIR
+    mkdir $DB_DIR && chown -R ldap: $DB_DIR
+}
+
+function restore_db(){
+    db_lock="$DB_DIR"'/.slapd_db.lock'
+    [[ $RESTORE_DB_ALWAYS -eq 1 || ! -f $db_lock ]] && remove_db
+    if [[ ! -f $db_lock ]]; then
+        printf 'Restaurando base de datos...\n'
+        su - -s /bin/bash ldap \
+            -c '/usr/sbin/slapadd -n 1 -F '"$SLAPD_CONF_DIR"' -l '"$RESTORE_DB_FILE"
+        (( $? )) || touch $db_lock
+    fi
+}
+
+function restore_ldap(){
+    if [[ -f $RESTORE_CONF_FILE ]]; then
+        restore_config
+    fi
+    if [[ -f $RESTORE_DB_FILE ]]; then
+        restore_db
+    fi
+}
+
 function init_db(){
     printf 'Comprobación de variables...\n'
     check_vars
@@ -93,7 +152,9 @@ function main(){
     printf 'Comprobación de los directorios..\n'
     check_dirs
     # Si se especifica o si no hay base de datos, se inicializa la BBDD de LDAP
-    if [[ ! -z $INIT_DB || $(ls $SLAPD_DIR | wc -l) -eq 0 ]]; then
+    if [[ -f $RESTORE_DB_FILE || -f $RESTORE_CONF_FILE ]]; then
+        restore_ldap
+    elif [[ ! -z $INIT_DB || $(ls $SLAPD_DIR | wc -l) -eq 0 ]]; then
         printf 'Iniciación de la base de datos...\n'
         init_db
     fi
--- services/purge_all.sh
+++ services/purge_all.sh
@@ -2,12 +2,7 @@
 
 COMPOSEFILE=$1
 
-if [[ ! -z $COMPOSEFILE ]]; then
-    [[ -f $COMPOSEFILE ]] && ARGS="-f $COMPOSEFILE" || exit 1
-fi
-
-docker-compose $ARGS stop
 docker rm $(docker ps -aq)
-docker images rm $(docker image ls -aq)
+docker image rm $(docker image ls -aq)
 docker volume rm $(docker volume ls -q)
 docker network prune
